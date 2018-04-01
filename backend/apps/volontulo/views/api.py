6d227f41f3fdf9576e58f94971593669e04496b9
jacekkalbarczyk <kalbarczyk@gmail.com>
2018-04-01 19:16:22 +0200

    MovingRegistrationToAngular7

--- backend/apps/volontulo/views/api.py
+++ backend/apps/volontulo/views/api.py
@@ -19,7 +19,6 @@ from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.utils import IntegrityError
-from django.shortcuts import redirect
 from rest_framework import status
 from rest_framework.decorators import api_view, detail_route
 from rest_framework.decorators import authentication_classes
